137762ea10f43fe28bf800e615c92db638615639
Luan Braz <luan_silvacpv@hotmail.com>
2022-07-26 11:29:09 -0300

    implementando Metodo create

--- src/main/java/com/luan/todo/resources/TodoResource.java
+++ src/main/java/com/luan/todo/resources/TodoResource.java
@@ -4,11 +4,10 @@ import com.luan.todo.domain.Todo;
 import com.luan.todo.services.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -40,6 +39,13 @@ public class TodoResource {
     public ResponseEntity<List<Todo>> listAll(){
         List<Todo> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<Todo> create(@RequestBody Todo obj){
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
--- src/main/java/com/luan/todo/services/TodoService.java
+++ src/main/java/com/luan/todo/services/TodoService.java
@@ -35,4 +35,9 @@ public class TodoService {
         List<Todo> list = repository.findAll();
         return list;
     }
+
+    public Todo create(Todo obj) {
+        obj.setId(null);
+        return repository.save(obj);
+    }
 }
