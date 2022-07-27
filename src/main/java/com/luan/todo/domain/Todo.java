d38ffc0845156a9dd9e57b6944da3626744ff096
Luan Braz <luan_silvacpv@hotmail.com>
2022-07-27 15:27:20 -0300

    ajustes e atualizações

--- src/main/java/com/luan/todo/domain/Todo.java
+++ src/main/java/com/luan/todo/domain/Todo.java
@@ -1,11 +1,13 @@
 package com.luan.todo.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
@@ -17,14 +19,16 @@ public class Todo implements Serializable {
     private Integer id;
     private String titulo;
     private String descricao;
-    private LocalDateTime dataParaFinalizar;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date dataParaFinalizar;
     private Boolean finalizado = false;
 
     public Todo() {
         super();
     }
 
-    public Todo(Integer id, String titulo, String descricao, LocalDateTime dataParaFinalizar, Boolean finalizado) {
+    public Todo(Integer id, String titulo, String descricao, Date dataParaFinalizar, Boolean finalizado) {
         this.id = id;
         this.titulo = titulo;
         this.descricao = descricao;
@@ -56,11 +60,11 @@ public class Todo implements Serializable {
         this.descricao = descricao;
     }
 
-    public LocalDateTime getDataParaFinalizar() {
+    public Date getDataParaFinalizar() {
         return dataParaFinalizar;
     }
 
-    public void setDataParaFinalizar(LocalDateTime dataParaFinalizar) {
+    public void setDataParaFinalizar(Date dataParaFinalizar) {
         this.dataParaFinalizar = dataParaFinalizar;
     }
 
--- src/main/java/com/luan/todo/services/DBService.java
+++ src/main/java/com/luan/todo/services/DBService.java
@@ -5,6 +5,8 @@ import com.luan.todo.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
@@ -15,13 +17,13 @@ public class DBService {
     @Autowired
     private TodoRepository todoRepository;
 
-    public void instanciaBaseDeDados(){
-        DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    public void instanciaBaseDeDados() throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
 
-        Todo t1 = new Todo(null, "Estudar", "Estudar java e angular", LocalDateTime.parse("25/03/2022 10:40", formatter), false);
-        Todo t2 = new Todo(null, "Ler", "Ler um livro", LocalDateTime.parse("25/07/2022 10:40", formatter), true);
-        Todo t3 = new Todo(null, "Correr", "Correr 20 km", LocalDateTime.parse("02/08/2022 10:40", formatter), false);
-        Todo t4 = new Todo(null, "Organizar", "Organizar a cassa", LocalDateTime.parse("15/08/2022 10:40", formatter), false);
+        Todo t1 = new Todo(null, "Estudar", "Estudar java e angular", sdf.parse("25/03/2022"), false);
+        Todo t2 = new Todo(null, "Ler", "Ler um livro", sdf.parse("25/07/2022"), true);
+        Todo t3 = new Todo(null, "Correr", "Correr 20 km", sdf.parse("02/08/2022"), false);
+        Todo t4 = new Todo(null, "Organizar", "Organizar a cassa", sdf.parse("15/08/2022"), false);
 
         todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
     }
